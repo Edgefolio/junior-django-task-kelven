4b38cf20f9811d1c390d394207a294bc47f199f9
KelvenH <kelvenhughes@me.com>
2022-05-01 16:18:29 +0000

    Initial development of strategy filter

--- funds/views.py
+++ funds/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Fund
 
 
@@ -9,7 +10,21 @@ def home(request):
 def view_funds(request):
     """ View to display all funds """
     funds = Fund.objects.all()
+    funds_num = Fund.objects.all().count()
+    strat_filter = None
+    
+    """filter data by strategy"""
+    if 'strategy_filter' in request.GET:
+        strat_filter = request.GET['strategy_filter']
+        filtered = Q(strategy__icontains=query)
+        funds = funds.filter(filtered)
+
+        print("Form get requested")
+
     context = {
-        'funds': funds
+        'funds': funds,
+        'funds_num': funds_num,
+        'filter': strat_filter
+        
     }
     return render(request, 'funds/funds.html', context)
